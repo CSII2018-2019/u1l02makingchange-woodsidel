1ca7e8b5e5fd5c110e4e89fb8bc8eefef5607d2a
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2018-09-25 13:54:12 -0400

    Not finished

--- src/U1L02MakingChange.java
+++ src/U1L02MakingChange.java
@@ -6,18 +6,20 @@ public class U1L02MakingChange {
 
 	public static void main(String[] args) {
 		
-		int numQuarter = 0;
-		int numDime = 0;
-		int numNickel = 0;
-		int numPenny = 0;
+		double numQuarter = 0;
+		double numDime = 0;
+		double numNickel = 0;
+		double numPenny = 0;
 		JOptionPane.showMessageDialog(null, "Welcome to Making Change!");
 		String number = JOptionPane.showInputDialog("Enter a monetary value (ex. 1.56): ");
 		double money = Double.parseDouble(number);
 		numPenny = (int)(money)*100;
-		numQuarter = (int)(numPenny)%25;
-		numDime = (int)(numQuarter)%10;
-		numNickel = (int)(numDime)%5;
-		numPenny = (int)(numNickel)%1;
+		(int)(numPenny)/25 = numQuarter;
+		numPenny%=25;
+		numQuarter/10 = numDime;
+		numPenny%=10;
+		
+		
 			
 
 		JOptionPane.showMessageDialog(null,"There are "+numQuarter+" quarters, "+numDime+" dimes, "
